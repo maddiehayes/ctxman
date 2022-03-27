8f37557e556362f22f27a0931a2b9a8093d2c4cf
Maddie Hayes <maddieh@squareup.com>
2022-03-27 18:23:07 -0400

    define app within package

--- internal/app/app.go
+++ internal/app/app.go
@@ -16,82 +16,84 @@ import (
 )
 
 var (
-	app *cli.App
 	cfg *config.Config
 )
 
-func Run() error {
-	app = &cli.App{
-		Name:  "ctx",
-		Usage: "Manage your shell environment context",
-		Before: func(c *cli.Context) error {
-			// Enable debug logs
-			if c.Bool("debug") {
-				log.SetLevel(log.DebugLevel)
+var app *cli.App = &cli.App{
+	Name:  "ctx",
+	Usage: "Manage your shell environment context",
+	Before: func(c *cli.Context) error {
+		// Enable debug logs
+		if c.Bool("debug") {
+			log.SetLevel(log.DebugLevel)
+		}
+		// Load config file
+		cfgFile := c.String("config")
+		log.Debugf("using config file: %q\n", cfgFile)
+		viper.SetConfigFile(cfgFile)
+		cfg = &config.Config{}
+		if err := viper.ReadInConfig(); err != nil {
+			return err
+		}
+		if err := viper.Unmarshal(cfg); err != nil {
+			return err
+		}
+		if err := cfg.Validate(); err != nil {
+			return err
+		}
+		// Add Config to context
+		c.Context = gocontext.WithValue(c.Context, config.CliContextKey, cfg)
+		return nil
+	},
+	Action: func(c *cli.Context) error {
+		// If --current, print the current context and exit
+		if c.Bool("current") {
+			ctx := context.Current()
+			if ctx != nil {
+				fmt.Println(*ctx)
+			} else {
+				log.Warn("no current context")
 			}
-			// Load config file
-			cfgFile := c.String("config")
-			log.Debugf("using config file: %q\n", cfgFile)
-			viper.SetConfigFile(cfgFile)
-			cfg = &config.Config{}
-			if err := viper.ReadInConfig(); err != nil {
-				log.Fatal(err)
-			}
-			if err := viper.Unmarshal(cfg); err != nil {
-				log.Fatal(err)
-			}
-			if err := cfg.Validate(); err != nil {
-				return err
-			}
-			// Add Config to context
-			c.Context = gocontext.WithValue(c.Context, config.CliContextKey, cfg)
 			return nil
+		}
+		// Else, use fzf to select from existing contexts
+		// TODO: fzf select context
+		log.Debug("auto-complete to set current context")
+		log.Warn("`ctx` fuzzy finder is unimplemented")
+		return nil
+	},
+	Commands: []*cli.Command{
+		commands.UseCmd,
+		list.ListCmd,
+	},
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:      "config",
+			Aliases:   []string{"f"},
+			Usage:     "load configuration from `FILE`",
+			Value:     defaultCfgFile(),
+			TakesFile: true,
+			Hidden:    true,
 		},
-		Action: func(c *cli.Context) error {
-			// If --current, print the current context and exit
-			if c.Bool("current") {
-				ctx := context.Current()
-				if ctx != nil {
-					fmt.Println(*ctx)
-				} else {
-					log.Warn("no current context")
-				}
-				return nil
-			}
-			// Else, use fzf to select context
-			// TODO: fzf select context
-			log.Debug("auto-complete to set current context")
-			return nil
+		&cli.BoolFlag{
+			Name:    "current",
+			Aliases: []string{"c"},
+			Usage:   "Print name of current context",
 		},
-		Commands: []*cli.Command{
-			commands.UseCmd,
-			list.ListCmd,
+		&cli.BoolFlag{
+			Name:  "debug",
+			Usage: "Enable debug logs",
 		},
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:      "config",
-				Aliases:   []string{"f"},
-				Usage:     "load configuration from `FILE`",
-				Value:     defaultCfgFile(),
-				TakesFile: true,
-				Hidden:    true,
-			},
-			&cli.BoolFlag{
-				Name:    "current",
-				Aliases: []string{"c"},
-				Usage:   "Print name of current context",
-			},
-			&cli.BoolFlag{
-				Name:  "debug",
-				Usage: "Enable debug logs",
-			},
-		},
-		// ExitErrHandler handles errors returned from any cli action
-		ExitErrHandler: func(context *cli.Context, err error) {
+	},
+	// ExitErrHandler handles errors returned from any cli action
+	ExitErrHandler: func(context *cli.Context, err error) {
+		if err != nil {
 			log.Fatal(err)
-		},
-	}
+		}
+	},
+}
 
+func Run() error {
 	return app.Run(os.Args)
 }
 
